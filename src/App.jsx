8b6353ebe497442f048e6b0b4d2672bc8da44425
Mehmet <mehmetimdat.py@gmail.com>
2024-03-23 23:48:54 +0300

    btn tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ const App = () => {
     return (
         <div>
             <div>
-                <Button success primary>Click me!</Button>
+                <Button success outline>Click me!</Button>
             </div>
             <div>
-                <Button danger>Buy Now!</Button>
+                <Button danger rounded>Buy Now!</Button>
             </div>
             <div>
                 <Button warning>See Now!</Button>
             </div>
             <div>
-                <Button danger>Hello</Button>
+                <Button secondary>Hello</Button>
             </div>
             <div>
-                <Button secondary>UP Down</Button>
+                <Button primary>UP Down</Button>
             </div>
         </div>
     );
